d5598f45bddebf18221cccbcd772c8a569dfde98
Iamqaasim <iamqaasim@gmail.com>
2023-03-15 19:56:11 +0000

    added nav bar items and links

--- website/views.py
+++ website/views.py
@@ -11,3 +11,27 @@ views = Blueprint('views',__name__)
 @views.route('/')
 def home():
   return render_template("home.html")
+
+@views.route('/mind')
+def mind():
+  return render_template("mind.html")
+
+@views.route('/body')
+def body():
+  return render_template("body.html")
+
+@views.route('/soul')
+def soul():
+  return render_template("soul.html")
+
+@views.route('/finance')
+def finance():
+  return render_template("finance.html")
+
+@views.route('/network')
+def network():
+  return render_template("network.html")
+
+@views.route('/ai_tools')
+def ai_tools():
+  return render_template("ai_tools.html")
